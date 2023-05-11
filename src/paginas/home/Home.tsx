f1541aec793c123c122c58e77a8753cd01ad2e19
Daisy Santos <daisy.santos@sumup.com>
2023-05-11 15:18:54 -0300

    Aula React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+function Home(){
+    return(
+    <>
+        <h1 className='titulo'>Desenvolvedora Java </h1>
+        <img className= 'imag' src="https://s-media-cache-ak0.pinimg.com/originals/93/23/dc/9323dc3e3edb993053c382e8bf11667a.jpg" alt= "" />
+        
+    </>
+    )
+}
+
+export default Home;
